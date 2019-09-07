3803910e348f31d7f2114c16443db92cfc8c68a9
Swati Wadhera <wadheraswati@gmail.com>
2019-09-07 19:03:46 +0530

    added keyboard methods and textfield delegate

--- McKinleyTest/ViewController.swift
+++ McKinleyTest/ViewController.swift
@@ -17,6 +17,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+
         // Do any additional setup after loading the view.
     }
     
@@ -41,7 +44,52 @@ class ViewController: UIViewController {
             })
         }
     }
-
-
+    
+    // MARK: - Keyboard Notifications -
+    @objc func keyboardWillShow(_ notification : NSNotification) {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keyboardRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keyboardRectangle.height
+//            contactDetailTVBottomConstraint.constant = -(keyboardHeight)
+//            contactDetailTV.updateConstraintsIfNeeded()
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification : NSNotification) {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+//        contactDetailTVBottomConstraint.constant = 0
+//        contactDetailTV.updateConstraintsIfNeeded()
+    }
 }
+
+private typealias TextField = ViewController
+extension TextField : UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == emailTF {
+            passwordTF.becomeFirstResponder()
+            return false
+        }
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if string == "\n" {
+            textField.resignFirstResponder()
+            return false
+        }
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+    }
+}
+
 
