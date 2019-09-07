fd2c6e0408fcfa816ff18fe1f71fe41ed08e75ff
Swati Wadhera <wadheraswati@gmail.com>
2019-09-07 18:54:56 +0530

    open webview controller if token is available

--- McKinleyTest/AppDelegate.swift
+++ McKinleyTest/AppDelegate.swift
@@ -15,6 +15,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        if let _ = UserDefaults.standard.value(forKey: "token") {
+            
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            let initialViewController = storyboard.instantiateViewController(withIdentifier: "webviewVC")
+            
+            self.window?.rootViewController = initialViewController
+            self.window?.makeKeyAndVisible()
+        }
         // Override point for customization after application launch.
         return true
     }
