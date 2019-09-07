cb35fc74a7ceda180c07497f7540af52aa2feda8
Swati Wadhera <wadheraswati@gmail.com>
2019-09-07 17:40:53 +0530

    Initial Commit

---
+++ McKinleyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  McKinleyTest
+//
+//  Created by Swati Wadhera on 07/09/19.
+//  Copyright © 2019 Swati Wadhera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
